8a3aaff96ec7fee136c3293583696f1d2663c502
alessandroNessi <alessandronessi@msn.com>
2021-08-30 18:55:03 +0200

    added multitikets

--- script/script.js
+++ script/script.js
@@ -1,36 +1,47 @@
 // add addEventListener to buttons an select
 // document.getElementById("insertData").addEventListener("click", insertData());
-document.getElementById("deleteForm").addEventListener("click", deleteForm());
-document.getElementById("userAge").addEventListener("click", hideEmptyOption());
+var liElement="<li class=\"element\"><span class=\"nome\"></span><span class=\"offerta\"></span><span class=\"carrozza\"></span><span class=\"codice\"></span><span class=\"costo\"></span></li>"
+document.getElementById("deleteForm").addEventListener("click", function deleteForm(){
+    document.getElementById("tiketsRecap").innerHTML="<li class=\"element\"><h3>NOME PASSEGGERO</h3><h4>Offerta</h4><h4>Carrozza</h4><h4>Codice CP</h4><h4>Costo Biglietto</h4></li>";
+    document.getElementById("dataContainer").classList.add("d-none");
+    document.getElementById("insertData").innerHTML="Genera";
+    document.getElementById("secondTitle").classList.add("d-none");
+});
 
-function hideEmptyOption(){
+document.getElementById("userAge").addEventListener("focus", function hideEmptyOption(){
     document.getElementById("userAge").getElementsByTagName("option")[0].classList.add("d-none");
-}
+});
+
 document.getElementById("insertData").addEventListener("click", function insertData()
 {
     let userName = document.getElementById("userName").value;
-    let distance = document.getElementById("distance").value;
+    let distance = parseInt(document.getElementById("distance").value);
     let age = document.getElementById("userAge").value;
-    distance*=0.21;
-    document.getElementById("dataContainer").classList.remove("d-none");
-    // document.getElementById("tiketsRecap").innerHTML+="<li></li>"
-    document.getElementById("tiketsRecap").getElementsByClassName("nome")[0].innerHTML=userName;
-    if(age=="minorenne"){
-        age="Offerta Junior";
-        distance*=0.8;
-    }else if(age=="maggiorenne"){
-        age="Biglietto Standard";
-    }else{
-        age="Offerta Silver";
-        distance*=0.6;
+    document.getElementById("userName").value="";
+    document.getElementById("distance").value="";
+    document.getElementById("userAge").value="";
+    console.log(age);
+    if(userName!="" && !isNaN(distance) && age!="empty"){
+        let i=document.getElementById("tiketsRecap").getElementsByClassName("element").length-1;
+        document.getElementById("tiketsRecap").innerHTML+=liElement;
+        document.getElementById("insertData").innerHTML="Aggiungi passeggero";
+        distance*=0.21;
+        document.getElementById("dataContainer").classList.remove("d-none");
+        document.getElementById("secondTitle").classList.remove("d-none");
+        document.getElementById("tiketsRecap").getElementsByClassName("nome")[i].innerHTML=userName;
+        if(age=="minorenne"){
+            age="Offerta Junior";
+            distance*=0.8;
+        }else if(age=="maggiorenne"){
+            age="Biglietto Standard";
+        }else{
+            age="Offerta Silver";
+            distance*=0.6;
+        }
+        document.getElementById("tiketsRecap").getElementsByClassName("offerta")[i].innerHTML=age;
+        document.getElementById("tiketsRecap").getElementsByClassName("carrozza")[i].innerHTML=Math.floor(Math.random()*9+1);
+        document.getElementById("tiketsRecap").getElementsByClassName("codice")[i].innerHTML=Math.floor(Math.random()*100000);
+        distance*=0.21;
+        document.getElementById("tiketsRecap").getElementsByClassName("costo")[i].innerHTML=distance+"€";
     }
-    document.getElementById("tiketsRecap").getElementsByClassName("offerta")[0].innerHTML=age;
-    document.getElementById("tiketsRecap").getElementsByClassName("carrozza")[0].innerHTML=Math.floor(Math.random()*9+1);
-    document.getElementById("tiketsRecap").getElementsByClassName("codice")[0].innerHTML=Math.floor(Math.random()*100000);
-    distance*=0.21;
-    if(age)
-    document.getElementById("tiketsRecap").getElementsByClassName("costo")[0].innerHTML=distance+"€";
 });
-function deleteForm(){
-
-}
