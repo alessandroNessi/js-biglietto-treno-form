89fef0917f359d71d570b815817ca9960a238a94
alessandroNessi <alessandronessi@msn.com>
2021-08-30 16:57:17 +0200

    esercizio base fatto

--- script/script.js
+++ script/script.js
@@ -1,16 +1,36 @@
 // add addEventListener to buttons an select
-document.getElementById("insertData").addEventListener("click", insertData());
+// document.getElementById("insertData").addEventListener("click", insertData());
 document.getElementById("deleteForm").addEventListener("click", deleteForm());
 document.getElementById("userAge").addEventListener("click", hideEmptyOption());
 
 function hideEmptyOption(){
     document.getElementById("userAge").getElementsByTagName("option")[0].classList.add("d-none");
 }
-function insertData(){
+document.getElementById("insertData").addEventListener("click", function insertData()
+{
     let userName = document.getElementById("userName").value;
     let distance = document.getElementById("distance").value;
     let age = document.getElementById("userAge").value;
-}
+    distance*=0.21;
+    document.getElementById("dataContainer").classList.remove("d-none");
+    // document.getElementById("tiketsRecap").innerHTML+="<li></li>"
+    document.getElementById("tiketsRecap").getElementsByClassName("nome")[0].innerHTML=userName;
+    if(age=="minorenne"){
+        age="Offerta Junior";
+        distance*=0.8;
+    }else if(age=="maggiorenne"){
+        age="Biglietto Standard";
+    }else{
+        age="Offerta Silver";
+        distance*=0.6;
+    }
+    document.getElementById("tiketsRecap").getElementsByClassName("offerta")[0].innerHTML=age;
+    document.getElementById("tiketsRecap").getElementsByClassName("carrozza")[0].innerHTML=Math.floor(Math.random()*9+1);
+    document.getElementById("tiketsRecap").getElementsByClassName("codice")[0].innerHTML=Math.floor(Math.random()*100000);
+    distance*=0.21;
+    if(age)
+    document.getElementById("tiketsRecap").getElementsByClassName("costo")[0].innerHTML=distance+"€";
+});
 function deleteForm(){
 
 }
