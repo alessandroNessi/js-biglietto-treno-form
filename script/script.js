c9ec3278caa6aa9665422eca1a61f4760bb11171
alessandroNessi <alessandronessi@msn.com>
2021-08-31 13:36:20 +0200

    minor fixes

--- script/script.js
+++ script/script.js
@@ -1,36 +1,37 @@
 var liElement="<li class=\"element\"><span class=\"nome\"></span><span class=\"offerta\"></span><span class=\"carrozza\"></span><span class=\"codice\"></span><span class=\"costo\"></span></li>"
+var tiketsRecap = document.getElementById("tiketsRecap");
+var userAgeInput = document.getElementById("userAge");
+var userNameInput = document.getElementById("userName");
+var DistanceInput = document.getElementById("distance");
 document.getElementById("deleteForm").addEventListener("click", function deleteForm(){
-    document.getElementById("tiketsRecap").innerHTML="<li class=\"element\"><h3>NOME PASSEGGERO</h3><h4>Offerta</h4><h4>Carrozza</h4><h4>Codice CP</h4><h4>Costo Biglietto</h4></li>";
+    tiketsRecap.innerHTML="<li class=\"element\"><h3>NOME PASSEGGERO</h3><h4>Offerta</h4><h4>Carrozza</h4><h4>Codice CP</h4><h4>Costo Biglietto</h4></li>";
     document.getElementById("dataContainer").classList.add("d-none");
     document.getElementById("insertData").innerHTML="Genera";
     document.getElementById("secondTitle").classList.add("d-none");
 });
 
-document.getElementById("userAge").addEventListener("focus", function hideEmptyOption(){
-    document.getElementById("userAge").getElementsByTagName("option")[0].classList.add("d-none");
-});
-
 document.getElementById("insertData").addEventListener("click", function insertData()
 {
-    let userName = document.getElementById("userName").value;
-    let distance = parseInt(document.getElementById("distance").value);
-    let age = document.getElementById("userAge").value;
-    document.getElementById("userName").value="";
-    document.getElementById("distance").value="";
-    document.getElementById("userAge").value="";
-    console.log(age);
-    if(userName!="" && !isNaN(distance) && age!="empty"){
-        let i=document.getElementById("tiketsRecap").getElementsByClassName("element").length-1;
+    let userName = userNameInput.value;
+    let distance = parseInt(DistanceInput.value);
+    let age = userAgeInput.value;
+    userNameInput.value="";
+    DistanceInput.value="";
+    userAgeInput.value="";
+    userAgeInput.selectedIndex = "0";
+    userAgeInput.focus();
+    if(userName!="" && !isNaN(distance) && age!="empty" && distance>0){
+        let i=tiketsRecap.getElementsByClassName("element").length-1;
         if(i==-1){
             i=0;
-            document.getElementById("tiketsRecap").innerHTML="<li class=\"element\"><h3>NOME PASSEGGERO</h3><h4>Offerta</h4><h4>Carrozza</h4><h4>Codice CP</h4><h4>Costo Biglietto</h4></li>";
+            tiketsRecap.innerHTML="<li class=\"element\"><h3>NOME PASSEGGERO</h3><h4>Offerta</h4><h4>Carrozza</h4><h4>Codice CP</h4><h4>Costo Biglietto</h4></li>";
         }
-        document.getElementById("tiketsRecap").innerHTML+=liElement;
+        tiketsRecap.innerHTML+=liElement;
         document.getElementById("insertData").innerHTML="Aggiungi passeggero";
         distance*=0.21;
         document.getElementById("dataContainer").classList.remove("d-none");
         document.getElementById("secondTitle").classList.remove("d-none");
-        document.getElementById("tiketsRecap").getElementsByClassName("nome")[i].innerHTML=userName;
+        tiketsRecap.getElementsByClassName("nome")[i].innerHTML=userName;
         if(age=="minorenne"){
             age="Offerta Junior";
             distance*=0.8;
@@ -40,10 +41,10 @@ document.getElementById("insertData").addEventListener("click", function insertD
             age="Offerta Silver";
             distance*=0.6;
         }
-        document.getElementById("tiketsRecap").getElementsByClassName("offerta")[i].innerHTML=age;
-        document.getElementById("tiketsRecap").getElementsByClassName("carrozza")[i].innerHTML=Math.floor(Math.random()*9+1);
-        document.getElementById("tiketsRecap").getElementsByClassName("codice")[i].innerHTML=Math.floor(Math.random()*100000);
-        distance*=0.21;
-        document.getElementById("tiketsRecap").getElementsByClassName("costo")[i].innerHTML=distance+"€";
+        tiketsRecap.getElementsByClassName("offerta")[i].innerHTML=age;
+        tiketsRecap.getElementsByClassName("carrozza")[i].innerHTML=Math.floor(Math.random()*9+1);
+        tiketsRecap.getElementsByClassName("codice")[i].innerHTML=Math.floor(Math.random()*90000+10000);
+        distance = (distance*0.21).toFixed(2);
+        tiketsRecap.getElementsByClassName("costo")[i].innerHTML=distance+"€";
     }
 });
